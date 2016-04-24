993b58028be53d0c59c36f845d91ffde1d093e87
NgocThien <pn.thien123@gmail.com>
2016-04-24 15:27:27 +0700

    cập nhật file models/user.js cho chức năng đăng nhập

--- models/user.js
+++ models/user.js
@@ -3,10 +3,6 @@ var mongoose = require('mongoose'),
 var bcrypt = require('bcryptjs');
 mongoose.connect('mongodb://localhost/udpt');
 var UserSchema = new Schema({
-    username: {
-        type: String,
-        index: true
-    },
     password: {
         type: String
     },
@@ -22,17 +18,17 @@ var UserSchema = new Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    bcrypt.genSalt(10, function(err, salt) {
+    /*bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(newUser.password, salt, function(err, hash) {
             newUser.password = hash;
             newUser.save(callback);
         });
-    });
+    });*/
+    newUser.save(callback);
 }
 
-module.exports.getUserByUsername = function(username, callback){
-    var query = {username: username};
-    User.findOne(query, callback);
+module.exports.getUserByEmail = function(email, callback){
+    User.findOne(email, callback);
 }
 
 module.exports.getUserById = function(id, callback){
