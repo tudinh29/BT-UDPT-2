0cdb36b83551ac5e04171c9f782c2d66733ee5ac
tudinh29 <1312676@student.hcmus.edu.vn>
2016-04-25 10:52:49 +0700

    commit logout

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
-
+function ensureAuthenticated(req,res,next){
+  if (infoUser){
+    return next();
+  } else{
+      req.flash('error_message','not succes');
+      res.redirect('/users/login');
+  }
+}
 module.exports = router;
