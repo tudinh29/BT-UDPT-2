f98f104116dd05d7bad9b9848f825ca85f7a9fb7
NgocThien <pn.thien123@gmail.com>
2016-04-30 09:09:03 +0700

    Thêm jquery.js cho trang web

---
+++ public/javascripts/jquery.js
@@ -0,0 +1,8 @@
+function new_messages(){
+  setTimeout(function(){window.location.href = '/users/new_messages';}, 300)}
+
+function sent_messages(){
+  setTimeout(function(){window.location.href = '/users/sent_messages';}, 300)}
+
+function list_friend(){
+  setTimeout(function(){window.location.href = '/users/friend';}, 300)}
