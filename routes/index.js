bbae1fdd2735b962fd8d400f74972b7e0240082d
NgocThien <pn.thien123@gmail.com>
2016-04-30 09:08:33 +0700

    Cập nhật index.js và user.js trong routes

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,33 @@ infoUser = null;
 router.get('/',ensureAuthenticated, function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
+
 router.get('/users/index',ensureAuthenticated, function(req, res, next) {
     res.render('index',{
         'title': 'Messages'
     })
 });
+
+router.get('/users/new_messages',ensureAuthenticated, function(req, res, next) {
+    res.render('new_messages',{
+        'title': 'New messages'
+    })
+});
+
+
+router.get('/users/sent_messages',ensureAuthenticated, function(req, res, next) {
+    res.render('sent_messages',{
+        'title': 'Sent messages'
+    })
+});
+
+router.get('/users/friend',ensureAuthenticated, function(req, res, next) {
+    res.render('friend',{
+        'title': 'Friend'
+    })
+});
+
+
 function ensureAuthenticated(req,res,next){
   if (infoUser){
     return next();
